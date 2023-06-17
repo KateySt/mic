8de702631bfcc17fec2c1ae25f87f4c42e180e89
KateySt <kateystetsenko@gmail.com>
2023-06-17 23:02:27 +0300

    implement eureka

--- customer/src/main/java/org/example/customer/CustomerService.java
+++ customer/src/main/java/org/example/customer/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
         customerRepository.save(customer);
         //TODO: if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
